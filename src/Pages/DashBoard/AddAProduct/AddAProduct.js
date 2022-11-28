e16d2bf977850a7b99313232ae6ae2e77e7a5ad4
MdJahid-Hasan <md.jahidhasan.tex@gmail.com>
2022-11-28 14:19:10 +0600

    product adding form create

--- src/Pages/DashBoard/AddAProduct/AddAProduct.js
+++ src/Pages/DashBoard/AddAProduct/AddAProduct.js
@@ -2,8 +2,68 @@ import React from 'react';
 
 const AddAProduct = () => {
     return (
-        <div>
-            <h1>Add A Product</h1>
+        <div className='border border-secondary m-2 p-4'>
+            <h2 className='text-primary font-bold text-2xl text-center'>Add Your Product For Sale</h2>
+            <form >
+
+
+                <input
+                    name="name"
+                    type="text"
+                    defaultValue={`Name: `}
+                    disabled
+                    placeholder="Full Name"
+                    className="input input-bordered input-primary w-full mt-2 "
+                />
+
+                <input
+                    name="email"
+                    defaultValue={`Email: `}
+                    disabled
+                    type="email"
+                    placeholder="Email"
+                    className="input input-bordered input-primary w-full mt-2"
+                />
+
+                <input
+                    name="bookName"
+                    defaultValue={`Books Name: `}
+                    disabled
+                    type="text"
+                    placeholder=""
+                    className="input input-bordered input-primary w-full mt-2"
+                />
+
+                <input
+                    name="price"
+                    defaultValue={`Price:  tk`}
+                    disabled
+                    type="text"
+                    placeholder=""
+                    className="input input-bordered input-primary w-full mt-2"
+                />
+
+                <input
+                    name="phone"
+                    required
+                    type="text"
+                    placeholder="Give Your Phone Number"
+                    className="input input-bordered input-primary w-full mt-2"
+                />
+
+                <input
+                    name="location"
+                    required
+                    type="text"
+                    placeholder="Enter Meeting Location"
+                    className="input input-bordered input-primary w-full mt-2"
+                />
+
+                <input
+                    type="submit"
+                    className="input btn-primary input-primary w-full mt-2"
+                ></input>
+            </form>
         </div>
     );
 };
